b44cedae5c21f19b8a0cbe4710226ba78d365fb9
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2024-11-08 23:09:00 +0500

    1

--- script/App.js
+++ script/App.js
@@ -1,4 +1,7 @@
-
+const id1 = document.getElementById('id-1');
+const id2 = document.getElementById('id-2');
+const id3 = document.getElementById('id-3');
+const id4 = document.getElementById('id-4');
 
 const btn = document.querySelector('.btn-s');
 
@@ -10,3 +13,11 @@ btn.addEventListener('click' , ()=>{
 document.getElementById('download-btn').addEventListener('click', function() {
     alert("Your resume is downloading..."); 
 });
+
+
+function showDiv(boxId) {
+    const boxes = document.querySelectorAll('.container-11');
+    boxes.forEach(box => box.style.opacity = 0);
+    document.getElementById(boxId).style.opacity = 1;
+   
+}
